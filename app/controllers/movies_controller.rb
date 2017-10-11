98a8c7415fccf650a7fef72baad18750b7ee87fd
Marco Mercado <mercadomax15@gmail.com>
2017-10-11 21:18:07 +0000

    End of Section 4.4

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,12 @@
+class MoviesController < ApplicationController
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @movies = Movie.all
+  end
+end
